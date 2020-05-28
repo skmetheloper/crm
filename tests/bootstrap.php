2ea3301da10f53f3d8b39b8e598d76201259fc70
Shin Maung Maung <ethereal97@gmail.com>
2020-05-28 07:45:28 +0630

    Delete bootstrap.php

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require __DIR__ . '/../bootstrap/app.php';
-
-// $kernel = $app->make(Illuminate\Contracts\Foundation\Http\Kernel::class);
