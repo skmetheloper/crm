7e3b06b2612e3d09088b9f0ffece0ff69b8b6d5a
Shin Maung Maung <ethereal97@gmail.com>
2020-05-28 07:39:41 +0630

    Update and rename CreatesApplication.php to bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+// $kernel = $app->make(Illuminate\Contracts\Foundation\Http\Kernel::class);
